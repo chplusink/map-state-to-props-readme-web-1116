f808171b33b419df3a84df038b28cf405e5a8aae
Vic Lam <lam.vic.k@gmail.com>
2017-02-12 17:28:14 -0500

    Add react-redux provider and connections

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import shoppingListItemReducer from './reducers/shoppingListItemReducer'
+import App from './App'
+import './index.css'
+
+const store = createStore(shoppingListItemReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const div = document.createElement('root');
+  ReactDOM.render(<Provider store={store}>
+    <App store={store}/>
+  </Provider>, div);
 });
